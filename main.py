d64863d41036db90ed908616f0fdd2f8bef0bf93
MTaninaga <taninagamio@outlook.jp>
2020-04-01 11:15:15 +0900

    mainでやるべき処理の流れ記述

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from flask import *
+from flask import Flask, render_template, redirect, secret_key
+
+app= Flask(__name__)
+
+@app.route('/')
+
+def main():
+// スクレイピング処理pyの呼び出し
+
+// スクレイピング結果のパース？
+
+// GoogleMapAPIにKeyを渡す？
+
+// GoogleMap初期地設定？
+
+// GoogleMapに所在地渡す？
+
+// GoogleMapのマーカー設定？
+
