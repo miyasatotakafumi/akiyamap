687463c4a74b558f63aadd5ac66e0d19e86302b9
MTaninaga <taninagamio@outlook.jp>
2020-04-01 14:53:49 +0900

    機能分割修正版

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ app= Flask(__name__)
 @app.route('/')
 
 def main():
-// スクレイピング処理pyの呼び出し
+// スクレイピング処理pyの呼び出し　-> 管理者が手動でやる、main.pyではしない
+// 表示時には、すでにDBに値が入っている前提
+// DBから名前と緯度と経度の値をとる
 
-// スクレイピング結果のパース？
+// GoogleMapAPIにKeyを渡す
 
-// GoogleMapAPIにKeyを渡す？
+// HTMLに名前と緯度経度を渡す　表示はrender_templateを使う
 
-// GoogleMap初期地設定？
 
-// GoogleMapに所在地渡す？
+// GoogleMap初期地設定～表示まではHTML側で行う
+// 検索Boxからの入力受け取り、値チェック
 
-// GoogleMapのマーカー設定？
+// ページ更新？
+
+// 
+
 
